1930d915dbd1e829bed105dac02ca951b5c12f71
underratedpoet <danielmaksimencko@yandex.ru>
2022-04-28 10:43:20 +0300

    Изменения ввода и вывода, создание класса

--- practice1/practice1.cpp
+++ practice1/practice1.cpp
@@ -9,7 +9,16 @@
 #include <ctime>
 using namespace std;
 
-void doMerge(vector<string>& words, int left, int right)
+class ofwords
+{
+public:
+ 
+    int length = 0;
+    int count = 0;
+};
+
+
+void doMerge(vector<string> &words, int left, int right)
 {
     vector<string> mid_word;
     for (int i = 0; i <= (right - left); i++)
@@ -50,7 +59,7 @@ void doMerge(vector<string>& words, int left, int right)
 
 }
 
-void sortMerge(vector<string>& words, int left, int right)
+void sortMerge(vector<string> &words, int left, int right)
 {
     if (left != right)
     {
@@ -89,15 +98,23 @@ string redactString(string word)
     return word;
 }
 
-void recieveInput(vector<string>& words)
+void recieveInput(vector<string> &words,string &path)
 {
     ifstream orig_file;
-    orig_file.open("D:\\Отчеты\\2 семестр Учебная практика\\original1.txt");
-    if (!orig_file.is_open())
-    {
-        cout << "Error accessing the file." << endl;
-        return;
+    bool check = false;
+    while (check == false) {
+        check = true;
+        orig_file.open(path);
+        if (!orig_file.is_open())
+        {
+            cout << "Error accessing the file." << endl;
+            cout << "Введите полный путь к текстовому файлу" << endl;
+            getline(cin, path);
+            check = false;
+        }
     }
+
+
     while (!orig_file.eof())
     {
         string new_word;
@@ -110,87 +127,117 @@ void recieveInput(vector<string>& words)
     orig_file.close();
 }
 
-void recordResult(vector<string>& words)
+void recordResult(vector<string>& words, char origpathnum)
 {
     ofstream result;
-    result.open("D:\\Отчеты\\2 семестр Учебная практика\\result1.txt");
+    string txt = ".txt";
+    string respath = "D:\\practice_txt_files\\result";
+    respath += origpathnum + txt;
+    cout << respath;
+    result.open(respath);
     for (int i = 0; i < words.size(); i++)
         result << words[i] << " ";
     result.close();
 }
 
-int findInVector(vector< array <int, 2>  > vtr, int number)
+int findInVector(vector <ofwords> vtr, int number)
 {
     for (int i = 0; i < vtr.size(); i++)
     {
-        if (vtr[i][0] == number)
+        if (vtr[i].length == number)
             return i;
     }
     return -1;
 }
 
-void countAllLengths(vector<string>& words, vector<array <int, 2> >& word_length_count)
+void countAllLengths(vector<string> &words, vector<ofwords> &word_length_count)
 {
     for (int i = 0; i < words.size(); i++)
     {
         int finded = findInVector(word_length_count, words[i].length());
         if (finded == -1)
         {
-            array<int, 2> values = { words[i].length(), 1 };
-            word_length_count.push_back(values);
+            ofwords ofwords;
+            ofwords.length = words[i].length();
+            ofwords.count++;
+            word_length_count.push_back(ofwords);
         }
         else
         {
-            word_length_count[finded][1] += 1;
+            word_length_count[finded].count++;
         }
     }
 }
 
-void doAndRecordAnalysis(vector<string>& words, int time)
+void doAndRecordAnalysis(vector<string> &words, int time, string origpath)
 {
     ifstream orig_file;
     ofstream analysis;
-    analysis.open("D:\\Отчеты\\2 семестр Учебная практика\\analysis1.txt");
+    string txt = ".txt";
+    string anpath = "D:\\practice_txt_files\\analysis";
+    anpath += origpath[origpath.length() - 5] + txt;
+    cout << anpath;
+    analysis.open(anpath);
+    cout << "Исходный текст: " << endl;
     analysis << "Исходный текст: " << endl;
-    orig_file.open("D:\\Отчеты\\2 семестр Учебная практика\\original1.txt");
+    orig_file.open(origpath);
     while (!orig_file.eof())
     {
         string line;
         getline(orig_file, line);
+        cout << line << endl;
         analysis << line << endl;
     }
     orig_file.close();
+    cout << endl << "Вариант 18: латиница, по кол-ву символов в слове, по убыванию, учитывать числа, сортировка слиянием.\nКоличество слов: " << words.size()
+        << endl << "Время сортировки: " << time << " мс\nСтатистика (количество слов каждой длины):" << endl;
     analysis << endl << "Вариант 18: латиница, по кол-ву символов в слове, по убыванию, учитывать числа, сортировка слиянием.\nКоличество слов: " << words.size()
         << endl << "Время сортировки: " << time << " мс\nСтатистика (количество слов каждой длины):" << endl;
-    vector<array <int, 2> > word_length_count;
+    vector<ofwords> word_length_count;
     countAllLengths(words, word_length_count);
-    for (int i = 0; i < word_length_count.size(); i++)
+    for (int i = word_length_count.size()-1; i >= 0; i--)
     {
-
-        analysis << word_length_count[i][0] << " - " << word_length_count[i][1] << endl;
+        cout << word_length_count[i].length << " - " << word_length_count[i].count << endl;
+        analysis << word_length_count[i].length << " - " << word_length_count[i].count << endl;
     }
 }
 
 
 
-void PRACTICE()
+void doPractice()
 {
+    cout << "Введите полный путь к текстовому файлу" << endl;
+    string path;
+    getline(cin, path);
+
     vector <string> words;
 
-    recieveInput(words);
+    recieveInput(words,path);
 
     int time0 = clock();
     sortMerge(words, 0, words.size() - 1);
     int time = clock() - time0;
 
-    recordResult(words);
+    recordResult(words,path[path.length()-5]);
 
-    doAndRecordAnalysis(words, time);
+    doAndRecordAnalysis(words, time, path);
 
 }
 
 int main()
 {
-    setlocale(LC_ALL, "Russian");
-    PRACTICE();
+    setlocale(0, "");
+    doPractice();
 }
+
+/* 1 - 13160, 75
+   2 - 4306, 25
+   3 - 7309, 42 
+   4 - 
+   5 - 
+   6 - 
+   7 - 
+   8 - 
+   9 - 
+   10 - 
+*/
